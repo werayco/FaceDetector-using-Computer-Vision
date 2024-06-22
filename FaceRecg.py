94d2c111cdb7e8cb8a1e5f2f547cdfbbb1eb55ff
werayco <werayco@gmail.com>
2024-06-22 06:19:16 -0700

    Updated

--- FaceRecg.py
+++ FaceRecg.py
@@ -1,18 +1,22 @@
 import cv2 as cv
 import numpy as np
 from typing import List
+import os
+import matplotlib.pyplot as plt
 
 faceRec=cv.face.LBPHFaceRecognizer.create()
 
-models= ["Ayra","Blessing","Bruno","Kelvin"]
+direc=r"C:\Users\LENOVO-PC\Downloads\Images\IMAGG"
 
-faceRec.read(r"FaceRec.yml")
+models= os.listdir(direc)
+
+faceRec.read(r"TrainedRecg.yml")
 
 faceDec=cv.CascadeClassifier(r"TheFace.xml")
 
 '''This Function returns the region of interest of the detected image using the Haar Cascade Classifier'''
 
-img = cv.imread(r"C:\Users\LENOVO-PC\Downloads\IMAGG\Ayra\Ayra1.png") # Opening the image in grayscale and converting it to Matrix
+img = cv.imread(r"C:\Users\LENOVO-PC\Downloads\download (8).png") # Opening the image in grayscale and converting it to Matrix
 
 gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
@@ -24,13 +28,14 @@ for (x,y,w,h) in facList:
 
     label,conf = faceRec.predict(regOfInt)
 
-    cv.putText(img,str(models[label]),(50,50),fontFace=cv.FONT_HERSHEY_PLAIN,fontScale=1,color=(0,255,0),thickness=1)
+    cv.putText(img,str(models[label]),(x,h),fontFace=cv.FONT_HERSHEY_PLAIN,fontScale=1,color=(0,255,0),thickness=1)
 
     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=1)
 
     print(conf)
 
     cv.imshow("iimg",img)
+
 
 cv.waitKey(0)
 
